bb6bdb58d8ab49452a439d674bb4c0f2af2fbb5b
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2025-10-17 21:02:43 +0200

    add reducer function

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,15 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../Button/Button";
+import { setStatusFilter } from "../../redux/actions";
 import css from "./StatusFilter.module.css";
-import { useSelector } from "react-redux";
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
   const filter = useSelector((state) => state.filters.status);
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
 
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === "all"}>All</Button>
-      <Button selected={filter === "active"}>Active</Button>
-      <Button selected={filter === "completed"}>Completed</Button>
+      <Button
+        selected={filter === "all"}
+        onClick={() => handleFilterChange("all")}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === "active"}
+        onClick={() => handleFilterChange("active")}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === "completed"}
+        onClick={() => handleFilterChange("completed")}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
